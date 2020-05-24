57562413388b31ef4fd4d0eb04b910251eb80e09
Brad <braddiroff@icloud.com>
2020-05-24 14:55:48 -0400

    Brad made a function

--- GitKrakenTesterboy/GitKrakenTesterboy/ViewController.swift
+++ GitKrakenTesterboy/GitKrakenTesterboy/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func thisIsBradFunction() {
+        print("Brad made a function")
+    }
 }
 
